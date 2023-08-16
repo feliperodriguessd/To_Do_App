fceeac7b38ddad34819676aba3d9f32b38c0adb1
Felipe Santos <71713922+feliperodriguessd@users.noreply.github.com>
2023-08-16 08:58:33 -0300

    Add files via upload

---
+++ ToDoApp.py
@@ -0,0 +1,19 @@
+"""Create an App to keep track of tasks
+- Create a loop
+    - Display existing tasks
+    - Display an option to add new tasks
+    - Option to exit
+"""
+task = []
+
+while 
+    display(tasks[])
+
+    add = print(input("Add task (Y/N):"))
+
+    if add == 'y' or 'Y':
+        print(input("Task: "))
+    elif add == 'n' or 'N':
+        exit()
+    else:
+        print('invalid answer, plsease input Y for yes or N for no')
