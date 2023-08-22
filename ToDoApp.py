8f017d251b02e2d63a0051c94a2c40b9bc134460
Felipe Santos <71713922+feliperodriguessd@users.noreply.github.com>
2023-08-21 23:06:27 -0300

    Add files via upload

--- ToDoApp.py
+++ ToDoApp.py
@@ -4,17 +4,28 @@
     - Display an option to add new tasks
     - Option to exit
 """
-tasks = {}
+tasks = []
 
 while True:
-    print(tasks)
-    user_input = input("Press 'T' to add a new task. Press 'D' to delete a task. Press 'E' to exit")
+    user_input = input("Press 'T' to add a new task. Press 'E' to exit. Press 'D' to delete a task.")
 
-    if user_input == "T" or user_input == "t":
+    if user_input.lower() == "t":
         new_input = input("Type your new task: ")
         tasks.append(new_input)
-    elif user_input == "N" or user_input == "n":
-        print(tasks)
+        print("Tasks: ", tasks)
+    
+    elif user_input.lower() == "d":
+        delete_input = input("Type the number of the task you want to delete or 'E' to exit")
+        if delete_input.isdigit():
+            delete_index = int(delete_input) - 1
+            if 0 <= delete_index < len(tasks):
+                del tasks[delete_index]
+                print(tasks)
+        else:
+            print(tasks )
+
+    elif user_input.lower() == "e":
         break
+
     else:
         print("Invalid input. Please enter Y or N.")
