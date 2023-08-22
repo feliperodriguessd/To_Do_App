3a416bcb5b64451dabf1b1179ce82f5d46a1f27e
Felipe Santos <71713922+feliperodriguessd@users.noreply.github.com>
2023-08-22 11:18:01 -0300

    Add files via upload
    Working app with basic functions

--- ToDoApp.py
+++ ToDoApp.py
@@ -15,12 +15,17 @@ while True:
         print("Tasks: ", tasks)
     
     elif user_input.lower() == "d":
+        for index, task in enumerate(tasks, start=1):
+            print(f"Press {index} to delete '{task}'")
+
         delete_input = input("Type the number of the task you want to delete or 'E' to exit")
         if delete_input.isdigit():
             delete_index = int(delete_input) - 1
             if 0 <= delete_index < len(tasks):
                 del tasks[delete_index]
                 print(tasks)
+            else:
+                print(f"invalid Index, please type a number between 0 and",len(tasks))
         else:
             print(tasks )
 
@@ -28,4 +33,4 @@ while True:
         break
 
     else:
-        print("Invalid input. Please enter Y or N.")
+        print("Invalid input. Please enter 'T', 'D' or 'E'")
