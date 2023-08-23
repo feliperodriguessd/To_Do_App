ce43eaa4185a380eadd0370978f7c9e5c1cc11b4
Felipe Santos <71713922+feliperodriguessd@users.noreply.github.com>
2023-08-23 10:39:49 -0300

    Class and Function
    Updated to a better code using function and class

--- ToDoApp.py
+++ ToDoApp.py
@@ -3,34 +3,55 @@
     - Display existing tasks
     - Display an option to add new tasks
     - Option to exit
+
+    Implement Functions and Classes
 """
-tasks = []
+class TodoApp:
+    def __init__(self):
+        self.tasks = []
 
-while True:
-    user_input = input("Press 'T' to add a new task. Press 'E' to exit. Press 'D' to delete a task.")
+    def display_tasks(self):
+        print("Tasks:")
+        for index, task in enumerate(self.tasks, start=1):
+            print(f"{index}. {task}")
 
-    if user_input.lower() == "t":
-        new_input = input("Type your new task: ")
-        tasks.append(new_input)
-        print("Tasks: ", tasks)
-    
-    elif user_input.lower() == "d":
-        for index, task in enumerate(tasks, start=1):
-            print(f"Press {index} to delete '{task}'")
+    def add_task(self, new_task):
+        self.tasks.append(new_task)
+        print("Task added:", new_task)
 
-        delete_input = input("Type the number of the task you want to delete or 'E' to exit")
+    def delete_task(self):
+        self.display_tasks()
+        delete_input = input("Type the number of the task you want to delete or 'E' to exit: ")
+        if delete_input.lower() == "e":
+            return
         if delete_input.isdigit():
             delete_index = int(delete_input) - 1
-            if 0 <= delete_index < len(tasks):
-                del tasks[delete_index]
-                print(tasks)
+            if 0 <= delete_index < len(self.tasks):
+                deleted_task = self.tasks.pop(delete_index)
+                print("Task deleted:", deleted_task)
             else:
-                print(f"invalid Index, please type a number between 0 and",len(tasks))
+                print("Invalid index.")
         else:
-            print(tasks )
+            print("Invalid input.")
 
-    elif user_input.lower() == "e":
-        break
+    def run(self):
+        while True:
+            user_input = input("Press 'T' to add a new task. Press 'E' to exit. Press 'D' to delete a task: ")
+            
+            if user_input.lower() == "t":
+                new_input = input("Type your new task: ")
+                self.add_task(new_input)
+            
+            elif user_input.lower() == "d":
+                self.delete_task()
+            
+            elif user_input.lower() == "e":
+                print("Exiting the app.")
+                break
+            
+            else:
+                print("Invalid input. Please enter 'T', 'D', or 'E'.")
 
-    else:
-        print("Invalid input. Please enter 'T', 'D' or 'E'")
+if __name__ == "__main__":
+    app = TodoApp()
+    app.run()
